db61a19815d512053ca930ecd4a230ef900ac501
Purusottam Mohapatra <purusottammohapatra@Purusottams-MacBook-Pro.local>
2023-03-10 23:19:55 +0530

    customer service setup

---
+++ customer-services/src/test/java/com/purusottam/customerservices/CustomerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purusottam.customerservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
